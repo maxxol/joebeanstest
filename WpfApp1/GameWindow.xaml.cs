6314bf1459c3147185f965fc4a45471094a33a58
Youriscc <114670194+Youriscc@users.noreply.github.com>
2022-10-05 14:52:12 +0200

    Add existing file

---
+++ WpfApp1/GameWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Windows.Media.Media3D;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        int gravity = 55;
+        private bool moveLeft, moveRight, onTheGround, jump;
+        private DispatcherTimer gameTimer = new DispatcherTimer();
+        private List<Rectangle> itemsToRemove = new List<Rectangle>();
+        private const int playerSpeed = 10;
+
+        
+        public GameWindow()
+        {
+            InitializeComponent();
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            gameTimer.Tick += GameEngine;
+            gameTimer.Start();
+            MyCanvas.Focus();
+            ImageBrush imageBrush = new ImageBrush();
+            imageBrush.ImageSource = new BitmapImage(new Uri("E:\\MainProjectArcadeGame\\WpfApp1\\Images\\Mario2.png", UriKind.RelativeOrAbsolute));
+
+        }
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left) moveLeft = true;
+            if (e.Key == Key.Right) moveRight = true;
+            if (e.Key == Key.Space)
+            {
+                jump = true;
+            }
+
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left) moveLeft = false;
+            if (e.Key == Key.Right) moveRight = false;
+            if (e.Key == Key.Space) jump = false;
+            
+
+        }
+
+        private void GameEngine(object sender, EventArgs e)
+        {
+            if (Canvas.GetTop(Player) < 720)
+            {
+                Canvas.SetTop(Player, Canvas.GetTop(Player) + gravity);
+            }
+            if (Canvas.GetTop(Player) == 720)
+            {
+                onTheGround = true;
+            }
+
+
+            if (jump == true)
+            {
+                Canvas.SetTop(Player, Canvas.GetTop(Player) - 55);
+            }
+            
+
+
+            if (moveLeft && Canvas.GetLeft(Player) > 0)
+                Canvas.SetLeft(Player, Canvas.GetLeft(Player) - playerSpeed);
+            if (moveRight && Canvas.GetLeft(Player) + Player.Width < Application.Current.MainWindow.Width)
+                Canvas.SetLeft(Player, Canvas.GetLeft(Player) + playerSpeed);
+            Rect playerHitBox = new Rect(Canvas.GetLeft(Player), Canvas.GetTop(Player), Player.Width, Player.Height);
+        }
+    }
+}
+
+
+
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SoundPlayer mediaPlayer = new SoundPlayer(@"E:\MainProjectArcadeGame\WpfApp1\Audio\mainMenuMusic.wav");
+        private int clickedtimes;
+
+        private string name = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+            mediaPlayer.Play();
+        }
+        private void NameInputButton_Click(object sender, RoutedEventArgs e)
+        {
+            name = MainTextBox.Text;
+            NameLabel.Content = $"Hallo, {name.ToString()} welkom bij de Albert Heijn game";
+        }
+
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            GameWindow gw = new GameWindow();
+            gw.Visibility = Visibility.Visible;
+        }
+
+        private void QuitButtonClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
