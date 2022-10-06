3043c08ea133f6c8a85ae3b8473cbc9216fcdff4
liambeintema <liambeintema@hotmail.com>
2022-10-06 14:33:06 +0200

    MainscrLayout

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameLayout
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private SoundPlayer mediaPlayer = new SoundPlayer(Properties.Resources.mainMenuMusic);
+        public MainWindow()
+        {
+            InitializeComponent();
+            mediaPlayer.Play();
+        }
+
+        private void QuitButtonClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
